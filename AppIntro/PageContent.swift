28b0fadb904381cb30e052db7257a60ca3e884bf
Valentina <v.o.butenko@gmail.com>
2016-09-07 11:38:50 +0300

    Starting project

---
+++ AppIntro/PageContent.swift
@@ -0,0 +1,18 @@
+//
+//  PageContent.swift
+//  AppIntro
+//
+//  Created by Valentina Butenko on 9/7/16.
+//  Copyright © 2016 ValentinaButenko. All rights reserved.
+//
+
+import UIKit
+
+class PageContent: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ AppIntro/PageVC.swift
@@ -0,0 +1,16 @@
+//
+//  PageVC.swift
+//  AppIntro
+//
+//  Created by Valentina Butenko on 9/7/16.
+//  Copyright © 2016 ValentinaButenko. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+ }
